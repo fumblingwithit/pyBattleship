5eb3fa6c8eca63643da89453af0e93fea9355c1f
Jon Brohauge <jonbrohauge@gmail.com>
2017-02-12 14:08:22 +0100

    Add shell in unit tests

--- test/test_board.py
+++ test/test_board.py
@@ -1,1 +1,11 @@
-#Unit tests of the board goes here
+# Unit tests of the board goes here
+import unittest
+
+
+class BoardTest(unittest.TestCase):
+
+    def test_board(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_ship.py
+++ test/test_ship.py
@@ -1,1 +1,11 @@
-#Unit tests of the ship goes here
+# Unit tests of the ship goes here
+import unittest
+
+
+class ShipTest(unittest.TestCase):
+
+    def test_ship(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
