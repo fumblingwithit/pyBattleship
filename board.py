502bae66c4948351fc02131ecece4cd4baac7123
Jon Brohauge <jonbrohauge@gmail.com>
2017-02-15 09:56:06 +0100

    Add functionality

--- board.py
+++ board.py
@@ -4,10 +4,52 @@ class Board:
         self.board = []
 
         for x in range(0, self.boardSize):
-            self.board.append(["O"] * self.boardSize)
+            value = str(x)
+            self.board.append([value] * self.boardSize)
 
     def is_on_board(self, x, y):
         if (0 <= x < self.boardSize) and (0 <= y < self.boardSize):
             return True
         else:
             return False
+
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def place_ship(self, ship):
+        if self.is_on_board(ship.startX, ship.startY):
+            if ship.orientation == "V":
+                x = ship.startX
+                for x in range(ship.startX, ship.startX+ship.size):
+                    self.set_piece(x, ship.startY)
+            else:
+                print("startX: " + str(ship.startX))
+                print("startY: " + str(ship.startY))
+                y = ship.startY
+                for y in range(ship.startY, ship.startY + ship.size):
+                    self.set_piece(ship.startX, y)
+
+        return True
+
+    def set_piece(self, x, y):
+        try:
+            if not self.is_on_board(x, y):
+                raise Exception('not_on_board')
+            if self.is_piece_set(x, y):
+                raise Exception('piece_is_set')
+            self.board[x].insert(y, 'S')
+            temp_board1 = self.board[x][:y].copy()
+            temp_board2 = self.board[x][y:].copy()
+            temp_board1.pop()
+            temp_board1.extend(temp_board2)
+            self.board[x] = temp_board1
+        except ValueError as err:
+            print(err.args)
+
+    def is_piece_set(self, x, y):
+        if self.is_on_board(x, y):
+            if str(self.board[x][y-1]) == "S":
+                return True
+            else:
+                return False
