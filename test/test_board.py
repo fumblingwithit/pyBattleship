f5e267ab8207f0192c48f88fc9bd1cbd468e552f
Jon Brohauge <jonbrohauge@gmail.com>
2017-02-15 09:55:46 +0100

    Add more tests

--- test/test_board.py
+++ test/test_board.py
@@ -3,15 +3,24 @@ import board
 
 
 class BoardTest(unittest.TestCase):
-    board_size = 10
-    board.Board(board_size)
+
+    def setUp(self):
+        self.board_size = 10
+        self.testBoard = board.Board(self.board_size)
 
     def test_create_board(self):
-        self.assertEqual(BoardTest.board_size, board.Board(self.board_size).boardSize)
+        self.assertEqual(self.board_size, self.testBoard.boardSize)
 
     def test_is_on_board(self):
-        self.assertTrue(board.Board(self.board_size).is_on_board(2, 3))
-        self.assertFalse(board.Board(self.board_size).is_on_board(99, 99))
+        self.assertTrue(self.testBoard.is_on_board(2, 3))
+        self.assertFalse(self.testBoard.is_on_board(99, 99))
+
+    def test_is_piece_set(self):
+        self.testBoard.set_piece(2, 3)
+        self.assertTrue(self.testBoard.is_piece_set(2, 3))
+        self.assertFalse(self.testBoard.is_piece_set(2, 2))
+        self.assertFalse(self.testBoard.is_piece_set(3, 3))
+        self.assertRaises(Exception, self.testBoard.set_piece, 2, 3)
 
 if __name__ == '__main__':
     unittest.main()
