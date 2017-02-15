98d8eaf03aa79f695014c3411a461f58bbc89cae
Jon Brohauge <jonbrohauge@gmail.com>
2017-02-15 09:56:36 +0100

    Add a bit of game play

--- battleship.py
+++ battleship.py
@@ -1,16 +1,25 @@
 import board
 import ship
 
-demoBoardSize = 5
-demoBoard = board.Board(demoBoardSize)
+boardSize = 10
+playerBoard = board.Board(boardSize)
+computerBoard = board.Board(boardSize)
 hitX = 1
 hitY = 1
-ship1 = ship.Ship(0, 0, 3, 'V')
+ship1 = ship.Ship(2, 3, 4, 'V')
+ship2 = ship.Ship(5, 5, 3, 'H')
 
 
 def start_game():
     print("Let's play Battleship!")
-   # self.print_board(board)
+
+
+def print_game():
+    print('-------------------')
+    board.Board.print_board(computerBoard)
+    print('===================')
+    board.Board.print_board(playerBoard)
+    print('-------------------')
 
 
 class Battleship:
@@ -25,10 +34,11 @@ class Battleship:
             print('targetX: %s targetY: %s' % (targetX, targetY))
             return True
 
-    def print_board(board):
-        for row in board:
-            print(" ".join(row))
-
 # ------------------- MAIN -------------------
 
-print("Let's play Battleship!")
+if __name__ == '__main__':
+    start_game()
+    print_game()
+    playerBoard.place_ship(ship1)
+    playerBoard.place_ship(ship2)
+    print_game()
