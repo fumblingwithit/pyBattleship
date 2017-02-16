50b954e0632c279f8e93468f9cb453a56c09bf7d
Jon Brohauge <jonbrohauge@gmail.com>
2017-02-16 07:52:58 +0100

    Refactor variable names

--- ship.py
+++ ship.py
@@ -1,13 +1,13 @@
 class Ship:
 
-    def __init__(self, sX, sY, s, o):
-        self.startX = int(sX)
-        self.startY = int(sY)
+    def __init__(self, startX, startY, s, o):
+        self.startX = int(startX)
+        self.startY = int(startY)
         self.size = int(s)
         self.orientation = str(o)
         self.ship = []
         for ptr in range(0, self.size):
-          self.ship.append(["S"])
+          self.ship.append(['S'])
 
     def printShip(self):
         for row in self.ship:
