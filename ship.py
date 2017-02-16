80ea95f31d22b529ab8f9a52ee8caacc308d265d
Jon Brohauge <jonbrohauge@gmail.com>
2017-02-16 08:03:09 +0100

    Refactor variable names. Rename printShip method

--- ship.py
+++ ship.py
@@ -1,15 +1,15 @@
 class Ship:
 
-    def __init__(self, startX, startY, s, o):
+    def __init__(self, startX, startY, size, orientation):
         self.startX = int(startX)
         self.startY = int(startY)
-        self.size = int(s)
-        self.orientation = str(o)
+        self.size = int(size)
+        self.orientation = str(orientation)
         self.ship = []
         for ptr in range(0, self.size):
           self.ship.append(['S'])
 
-    def printShip(self):
+    def show_ship(self):
         for row in self.ship:
             print(" ".join(row))
 
