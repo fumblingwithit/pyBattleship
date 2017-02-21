4e551e8c534c89bd361dbb7930b102c3adb8abd0
Jon Brohauge <jonbrohauge@gmail.com>
2017-02-21 09:11:48 +0100

    Flip X and Y
    simplify game

--- ship.py
+++ ship.py
@@ -12,6 +12,13 @@ class Ship(object):
         for ptr in range(0, self.size):
             self.ship.append(['S'])
 
+    def debug(self, x_coordinate, y_coordinate, value):
+        print('===DEBUG===')
+        self.show()
+        print('X: ' + str(x_coordinate) + '(' + str(self.start_x) + ')')
+        print('Y: ' + str(y_coordinate) + '(' + str(self.start_y) + ')')
+        print('Value: ' + value)
+
     def show(self):
         """Shows the ship"""
         print('Name: ' + self.name)
@@ -28,7 +35,6 @@ class Ship(object):
                 return 'Hit'
             else:
                 board.update_piece(x_coordinate, y_coordinate, 'M')
-                self.update(x_coordinate, y_coordinate, 'M')
                 return 'Miss'
         else:
             return 'Dumb ASS! - you missed the board'
@@ -36,7 +42,7 @@ class Ship(object):
     def place(self, board):
         """Place a ship on the board"""
         if board.is_on_board(self.start_x, self.start_y):
-            if self.orientation == 'V':
+            if self.orientation == 'H':
                 x_coordinate = self.start_x
                 for x_coordinate in range(self.start_x, self.start_x + self.size):
                     board.place_piece(x_coordinate, self.start_y)
@@ -46,34 +52,10 @@ class Ship(object):
                     board.place_piece(self.start_x, y_coordinate)
 
     def update(self, x_coordinate, y_coordinate, value):
-        print('Ship: ' + str(self.ship))
-        print('Orientation: ' + self.orientation)
-        print('X: ' + str(x_coordinate))
-        print('Y: ' + str(y_coordinate))
-        print('Value: ' + value)
-
+        #self.debug(x_coordinate, y_coordinate, value)
         if self.orientation == 'H':
-            print('H - 1')
-            print('Start_X: ' + str(self.start_x))
-            print('Start_Y: ' + str(self.start_y))
-            print('X: ' + str(x_coordinate))
-            print('Y: ' + str(y_coordinate))
             if self.start_y == y_coordinate:
-                self._place_mark(x_coordinate, value)
+                self.ship[x_coordinate-self.start_x] = [value]
         else:
             if self.start_x == x_coordinate:
-                print('V - 1')
-                self._place_mark(y_coordinate, value)
-
-    def _place_mark(self, index, value):
-        print('---DEBUG MARK---')
-        print('Ship: ' + str(self.ship))
-        print('Orientation: ' + self.orientation)
-        print('Index: ' + str(index))
-        print('Value: ' + value)
-        self.ship.insert(index, [value])
-        temp_ship1 = self.ship[:index].copy()
-        temp_ship2 = self.ship[index:].copy()
-        temp_ship1.pop()
-        temp_ship1.extend(temp_ship2)
-        self.ship = temp_ship1.copy()
+                self.ship[y_coordinate-self.start_y] = [value]
